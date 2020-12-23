22cb325bca119c40aef6a980f9c4ae366a986acc
Przemyslaw Zygula <zygula.przemyslaw@gmail.com>
2020-12-23 21:10:11 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Liczy
+//Liczby
 
 const tab = [];
 
@@ -18,12 +18,16 @@ div.innerText = tab;
 var link1 = document.querySelector('#button1')
 var link2 = document.querySelector('#button2')
 var link3 = document.querySelector('#button3')
+var link4 = document.querySelector('#button4')
+var link5 = document.querySelector('#button5')
 
 name = " container__button--hidden";
 
 link1.onclick = style;
 link2.onclick = style;
 link3.onclick = style;
+link4.onclick = style;
+link5.onclick = style;
 
 function style () {
         this.className += name;
